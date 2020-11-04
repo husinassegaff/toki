3b17e057f2aaf37c5b6e91eda0cb625369e003e2
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2020-11-04 23:27:35 +0700

    solusi sementara bilangan agak prima

---
+++ Pemrograman Dasar/Analisis Kompleksitas/BilanganAgakPrima.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int N, x, temp = 0;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x;
+        bool isPrime = true;
+
+        if (x == 1) {
+            isPrime = true;
+        }
+        else {
+            for (int j = 2; j <= sqrt(x); j++) {
+                if (x % j == 0) {
+                    temp++;
+                    if(temp == 2){
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (isPrime)
+            cout << "YA" << endl;
+        else
+            cout << "BUKAN" << endl;
+    }
+    return 0;
+}
