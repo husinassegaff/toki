a42630cc32239dfa7d951595b72de6d51ae45205
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2020-11-06 22:42:02 +0700

    proses pengerjaan problem tukang sulap

---
+++ Pemrograman Dasar/Subprogram/TukangSulap.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+    int N;
+
+    cin >> N;
+
+    int A[N-1];
+    int B[N-1];
+
+    for (int i = 0; i <= N; ++i){
+        cin >> A[i];
+    }
+
+     for (int i = 0; i <= N; ++i){
+        cin >> B[i];
+    }
+}
